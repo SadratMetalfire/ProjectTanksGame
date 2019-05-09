fd3a18a81a60c0cec0dc021d3f2d6bd6ab1e5332
IPRO100I <47029423+IPRO100I@users.noreply.github.com>
2019-05-09 17:25:53 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,51 @@
 //Global variables
 static sf::Vector2f WindowSize;
 
+void GenerateMap(sf::ConvexShape &Polygon,int HorizontalOffset, int VerticalOffset){
+    int PointCount = static_cast<int>((WindowSize.x / HorizontalOffset) + 2);
+    int Reminder = static_cast<int>(WindowSize.x) % HorizontalOffset;
+    std::cout<<Reminder<<std::endl;
+    if(Reminder != 0){
+        PointCount += 1;
+    }
+    Polygon.setPointCount(static_cast<size_t>(PointCount + 1));
+
+    std::cout << PointCount << std::endl;
+
+    sf::Vector2f CurrentPointPosition = sf::Vector2f(0,WindowSize.y / 2);
+    sf::Vector2f LastPointPosition;
+
+    for(int i = 0 ; i <= PointCount ; i++){
+        if(i == 0){
+            Polygon.setPoint(static_cast<size_t>(i),CurrentPointPosition);
+        }
+        else if(i == PointCount - 1){
+            Polygon.setPoint(static_cast<size_t>(i),sf::Vector2f(WindowSize.x,WindowSize.y));
+        }else if(i == PointCount){
+            Polygon.setPoint(static_cast<size_t>(i),sf::Vector2f(0,WindowSize.y));
+        }else{
+            if((i == PointCount - 2) && Reminder != 0){
+                CurrentPointPosition.x = LastPointPosition.x + Reminder;
+            }else{
+                CurrentPointPosition.x = LastPointPosition.x + HorizontalOffset;
+            }
+            std::cout << CurrentPointPosition.x << std::endl;
+            if(rand()%2 > 0){
+                do{
+                    CurrentPointPosition.y = LastPointPosition.y - rand()%VerticalOffset;
+                }while(CurrentPointPosition.y >= 600 || CurrentPointPosition.y <= 200);
+            }else{
+                do{
+                    CurrentPointPosition.y = LastPointPosition.y + rand()%VerticalOffset;
+                }while(CurrentPointPosition.y >= 600 || CurrentPointPosition.y <= 200);
+            }
+            Polygon.setPoint(static_cast<size_t>(i),CurrentPointPosition);
+        }
+        LastPointPosition = CurrentPointPosition;
+    }
+    Polygon.setFillColor(sf::Color(static_cast<unsigned char>(rand()%256),static_cast<unsigned char>(rand()%256),static_cast<unsigned char>(rand()%256)));
+}
+
 int main()
 {
 
